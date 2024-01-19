7f411dddec6ccc434bddbd4602cf994aeeb77576
moises0814 <144181018+moises0814@users.noreply.github.com>
2024-01-19 16:13:22 -0600

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   apiUrl: `https://pokeapi.co/api/v2/`,
-  RTNurl: `http://api.amdc.hn:3000/apirtn`,
+  // RTNurl: `http://api.amdc.hn:3000/apirtn`,
+  RTNurl: `http://localhost:3000/apirtnl`,
   Ventaurl: `http://api.amdc.hn:3000/ventaBruta`,
   production: true
 };
